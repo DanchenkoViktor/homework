463727033ec7547f4897790a311e7f01ab87d3d5
viktordanchenko <meteor2433@gmail.com>
2022-09-29 02:24:00 +0300

    added isValidBSTTree

--- HW_L7_BSTValidator/T1_BSTValidator/src/main/java/ru/edu/TreeHelper.java
+++ HW_L7_BSTValidator/T1_BSTValidator/src/main/java/ru/edu/TreeHelper.java
@@ -13,6 +13,37 @@ public class TreeHelper {
      * @param array - array that contains tree.
      */
     public static boolean isValidBSTTree(Integer[] array) {
-        throw new RuntimeException("NOT IMPLEMENTED!!!!");
+        return isValidBSTTree(array, 0);
+    }
+
+    private static boolean isValidBSTTree(Integer[] data, int parent) {
+        if (data[parent] == null)
+            return true;
+
+        if (data.length == 1) {
+            return true;
+        }
+
+        if (leftChild(parent) < data.length && data[leftChild(parent)] != null && data[leftChild(parent)] > data[parent]) {
+            return false;
+        }
+        if (rightChild(parent) < data.length && data[rightChild(parent)] != null && data[rightChild(parent)] < data[parent]) {
+            return false;
+        }
+
+        if (leftChild(parent) < data.length && !isValidBSTTree(data, leftChild(parent))
+                || rightChild(parent) < data.length && !isValidBSTTree(data, rightChild(parent))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static int leftChild(int i) {
+        return (2 * i) + 1;
+    }
+
+    private static int rightChild(int i) {
+        return leftChild(i) + 1;
     }
 }
